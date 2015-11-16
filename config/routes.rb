5738eec7b7c6559e8fe1dd0aeb9b6001c4e025ca
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-16 14:55:19 -0500

    Refactoring routes and adding locals to render _form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :users, only: [:new, :create, :show, :edit, :update] do
+  resources :users do
     resources :reviews, only: [:show, :destroy]
   end
 
