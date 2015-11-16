a2b210c60b8a2fee0bb69a2d5d442381a55d262b
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-16 15:15:38 -0500

    can only edit the information in your own user profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+
+    if @user != current_user
+      redirect_to products_url, notice: "You cannot edit this user."
+    end
   end
 
   def update
